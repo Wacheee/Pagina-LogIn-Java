549d1db5a0b9b6f587256172029cf7c383952fa5
Ignacio Barria <ignacio.barria.o@gmail.com>
2021-01-26 17:52:25 -0300

    Agregado campo PASS, generalizado Metodos de WaterMark para ambos campos

--- src/main/java/Wache/Ventana.java
+++ src/main/java/Wache/Ventana.java
@@ -3,6 +3,7 @@ package Wache;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Image;
@@ -16,6 +17,7 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import javax.swing.SwingUtilities;
@@ -30,9 +32,10 @@ public class Ventana{
     JFrame miVentana = new JFrame("Autenticación"); //Recomiendan hacerlo privado, pero lo dejare public para jugar con sus metodos
     private JPanel panel1 = new JPanel();
     private JPanel panelNom = new JPanel();
-    private JPanel panelApe = new JPanel();
+    private JPanel panelPass = new JPanel();
     private JLabel titulo = new JLabel("Sistema de LogIn", SwingConstants.CENTER);
-    boolean vacio = true; //BOOLEANO PARA el WaterMark
+    boolean passVacio = true; //BOOLEANO PARA el WaterMark
+    boolean nomVacio = true;
    
     public Ventana(){ //Algunos dicen que en vez del constructor, dejarlo en un metodo
         miVentana.setTitle("Autenticación");
@@ -73,8 +76,8 @@ public class Ventana{
         titulo.setFont(new Font("verdana", Font.PLAIN, 30));
         //titulo.setAlignmentY(panel1.CENTER_ALIGNMENT);
         
-        //TextBox y Label Nombre
-        JTextField txtNombre = new JTextField("Ingrese su nombre");
+        //TextBox y Label Usuario
+        JTextField txtNombre = new JTextField("Ingrese usuario");
         txtNombre.setMaximumSize(new Dimension(250,300));
         txtNombre.setMinimumSize(new Dimension(250,0));
         txtNombre.setPreferredSize(new Dimension(250,20));
@@ -82,42 +85,45 @@ public class Ventana{
         txtNombre.setBorder(BorderFactory.createLineBorder(Color.gray, 1, false));
         txtNombre.setAlignmentX(panelNom.CENTER_ALIGNMENT);
         
-        JLabel nombre = new JLabel("Nombre:");
+        JLabel nombre = new JLabel("Usuario:");
 
         
         panelNom.setLayout(new BoxLayout(panelNom, BoxLayout.LINE_AXIS));
         
-        panelNom.add(Box.createHorizontalGlue());
+        panelNom.add(Box.createRigidArea(new Dimension(30,0)));
         panelNom.setOpaque(false);
         panelNom.add(nombre);
+       // panelNom.add(Box.createRigidArea(new Dimension(20,0)));
         panelNom.add(Box.createHorizontalGlue());
         panelNom.add(txtNombre);
-        panelNom.add(Box.createHorizontalGlue());
+        panelNom.add(Box.createRigidArea(new Dimension(30,0)));
         
         
         
         //TextBox y Label Apellido
-        JTextField txtApellido = new JTextField("Ingrese su apellido");
-        txtApellido.setMaximumSize(new Dimension(250,300));
-        txtApellido.setMinimumSize(new Dimension(250,0));
-        txtApellido.setPreferredSize(new Dimension(250,20));
-        txtApellido.setFont(new Font("verdana", Font.ITALIC, 12));
-        txtApellido.setForeground(Color.GRAY);
-        txtApellido.setBorder(BorderFactory.createLineBorder(Color.gray, 1, false));
+        JPasswordField txtPass = new JPasswordField("Ingrese contraseña");
+        txtPass.setMaximumSize(new Dimension(250,300));
+        txtPass.setEchoChar((char)0);
+        txtPass.setMinimumSize(new Dimension(250,0));
+        txtPass.setPreferredSize(new Dimension(250,20));
+        txtPass.setFont(new Font("verdana", Font.ITALIC, 12));
+        txtPass.setForeground(Color.GRAY);
+        txtPass.setBorder(BorderFactory.createLineBorder(Color.gray, 1, false));
         
-        txtApellido.setAlignmentX(panelApe.CENTER_ALIGNMENT);
+        txtPass.setAlignmentX(panelPass.CENTER_ALIGNMENT);
         
-        JLabel apellido = new JLabel("Apellido:");
+        JLabel pass = new JLabel("Contraseña:");
 
         
-        panelApe.setLayout(new BoxLayout(panelApe, BoxLayout.LINE_AXIS));
+        panelPass.setLayout(new BoxLayout(panelPass, BoxLayout.LINE_AXIS));
         
-        panelApe.add(Box.createHorizontalGlue());
-        panelApe.setOpaque(false);
-        panelApe.add(apellido);
-        panelApe.add(Box.createHorizontalGlue());
-        panelApe.add(txtApellido);
-        panelApe.add(Box.createHorizontalGlue());
+        panelPass.add(Box.createRigidArea(new Dimension(30,0)));
+        panelPass.setOpaque(false);
+        panelPass.add(pass);
+        panelPass.add(Box.createHorizontalGlue());
+        //panelPass.add(Box.createRigidArea(new Dimension(20,0)));
+        panelPass.add(txtPass);
+        panelPass.add(Box.createRigidArea(new Dimension(30,0)));
         
         
         
@@ -153,7 +159,7 @@ public class Ventana{
         panel1.add(Box.createVerticalGlue());
         panel1.add(panelNom, BorderLayout.CENTER);
         panel1.add(Box.createRigidArea(new Dimension(0,20)));
-        panel1.add(panelApe, BorderLayout.CENTER);
+        panel1.add(panelPass, BorderLayout.CENTER);
         panel1.add(Box.createVerticalGlue());
         panel1.add(boton, BorderLayout.CENTER);
         panel1.add(Box.createRigidArea(new Dimension(0,40)));
@@ -164,12 +170,13 @@ public class Ventana{
         miVentana.add(cajaContenedora);
         //miVentana.pack();
         
-        
+
         
            //WATERMARK CASERO!     --> El problema, al momento de Pegar, solo copia la primera letra       
            //EVENTO FOCUS
-        txtApellido.addFocusListener(new FocusListener() {
-            public void focusGained(FocusEvent e) {
+        txtPass.addFocusListener(new FocusListener() {
+            public void focusGained(FocusEvent e) { //Si hay focus y sale "Ingrese Apellido", dejamos el curso en 0.
+              /*  METODO ORIGINAL
                 if(txtApellido.getText().equals("Ingrese su apellido")){
                 txtApellido.setText("Ingrese su apellido");
                 txtApellido.setCaretPosition(0); //CARET ES LA POSICIÓN DEL CURSO DEL TEXTO
@@ -177,49 +184,70 @@ public class Ventana{
                 txtApellido.setFont(new Font("verdana", Font.ITALIC, 12));
                 txtApellido.setForeground(Color.GRAY);
                 }
+                    */
+              
+              passVacio = WTconFocus(e, txtPass, "Ingrese contraseña", passVacio);
+                if(passVacio == true){      //Para volver VISIBLE si es que NO se ha ingreado (para mostar WT)
+                txtPass.setEchoChar((char)0);
+            }
+
             }
 
     
-            public void focusLost(FocusEvent e) {
-            if(txtApellido.getText().equals("")){
+            public void focusLost(FocusEvent e) { //Si no se escribe nada y se va del texto
+            /* METODO ORINGIAL
+                if(txtApellido.getText().equals("")){
                  txtApellido.setText("Ingrese su apellido");
                  txtApellido.setFont(new Font("verdana", Font.ITALIC, 12));
                  vacio = true;
                  txtApellido.setForeground(Color.GRAY);
             }
-            
+                */
+            passVacio = WTsinFocus(e, txtPass, "Ingrese contraseña", passVacio);
+                         
+            if(passVacio == true){      //Para volver VISIBLE la contraseña si NO se ha ingreado constaseña (para mostar WT)
+                txtPass.setEchoChar((char)0);
+            }
             }
         });
         
         //EVENTO QUE ESCUCHA LO QUE ESCRIBE EL USUARIO.
-        txtApellido.getDocument().addDocumentListener(new DocumentListener() {
+        txtPass.getDocument().addDocumentListener(new DocumentListener() {
             @Override
             public void insertUpdate(DocumentEvent arg0) {
-                corroborar();                
+
+                 passVacio =WTcorroborar(txtPass, "Ingrese contraseña", passVacio); 
+                 if(passVacio == false){ //Para volver INVISIBLE la contraseña si se ha ingreado constaseña
+                 txtPass.setEchoChar('\u2022');
+              }
             }
 
             @Override
             public void removeUpdate(DocumentEvent arg0) {
-                corroborar();
-                          
+                passVacio = WTcorroborar(txtPass, "Ingrese contraseña", passVacio); 
+                    if(passVacio == false){ //Para volver INVISIBLE la contraseña si se ha ingreado constaseña
+                 txtPass.setEchoChar('\u2022');
+              }         
             }
 
             @Override
             public void changedUpdate(DocumentEvent arg0) {
-                corroborar();
-                
+                 passVacio = WTcorroborar(txtPass, "Ingrese contraseña", passVacio); 
+                   if(passVacio == false){ //Para volver INVISIBLE la contraseña si se ha ingreado constaseña
+                 txtPass.setEchoChar('\u2022');
+              }
             }
             
-            
+            /* METODO ANTIGUO
             private void corroborar(){ //PARA QUE FICNOE BUEN HAY Q HACER EL invokeLater.
                 Runnable doHighlight = new Runnable() {
                     
                     @Override
                     public void run() {
-                        if(!txtApellido.getText().equals("Ingrese su apellido") && vacio == true){
+                        if(!txtApellido.getText().equals("Ingrese contraseña") && vacio == true){
                             vacio = false;
                             String texto = txtApellido.getText();
-                            texto = texto.replace("Ingrese su apellido", "");
+                            texto = texto.replace("Ingrese contraseña", "");
                             //char letra = texto.charAt(0);
                             //txtApellido.setText(String.valueOf(letra));  
                             txtApellido.setText(texto);
@@ -230,11 +258,12 @@ public class Ventana{
                 };       
                 SwingUtilities.invokeLater(doHighlight);
             }
+            */
         });
         
         
         //EVENTO PARA CONTROLAR EL CARET (El cursor de texto), si esta el WaterMark, no puede mover el cursor
-        txtApellido.getCaret().addChangeListener(new ChangeListener(){     //Seams to work!
+        txtPass.getCaret().addChangeListener(new ChangeListener(){     //Seams to work!
 
             @Override
             public void stateChanged(ChangeEvent e) {
@@ -244,8 +273,69 @@ public class Ventana{
                     @Override
                     public void run() {
 
-                         if(txtApellido.getCaretPosition() != 0 && vacio == true){
-                   txtApellido.setCaretPosition(0);
+                         if(txtPass.getCaretPosition() != 0 && passVacio == true){
+                   txtPass.setCaretPosition(0);
+               }
+
+                    }
+                });
+            }
+        });
+
+        
+        
+        
+        
+        //Linstener y Eveentos caja nombre
+        txtNombre.addFocusListener(new FocusListener() {
+            public void focusGained(FocusEvent e) { //Si hay focus y sale "Ingrese Apellido", dejamos el curso en 0.            
+              nomVacio = WTconFocus(e, txtNombre, "Ingrese usuario", nomVacio);
+            }
+
+    
+            public void focusLost(FocusEvent e) { //Si no se escribe nada y se va del texto
+            nomVacio = WTsinFocus(e, txtNombre, "Ingrese usuario", nomVacio);
+            
+            }
+        });
+        
+        //EVENTO QUE ESCUCHA LO QUE ESCRIBE EL USUARIO.
+        txtNombre.getDocument().addDocumentListener(new DocumentListener() {
+            @Override
+            public void insertUpdate(DocumentEvent arg0) {
+                 nomVacio =WTcorroborar(txtNombre, "Ingrese usuario", nomVacio);   
+            }
+
+            @Override
+            public void removeUpdate(DocumentEvent arg0) {
+                nomVacio = WTcorroborar(txtNombre, "Ingrese usuario", nomVacio);   
+                          
+            }
+
+            @Override
+            public void changedUpdate(DocumentEvent arg0) {
+                 nomVacio = WTcorroborar(txtNombre, "Ingrese usuario", nomVacio);   
+                
+            }
+            
+          
+        });
+        
+        
+        //EVENTO PARA CONTROLAR EL CARET (El cursor de texto), si esta el WaterMark, no puede mover el cursor
+        
+        txtNombre.getCaret().addChangeListener(new ChangeListener(){     //Seams to work!
+
+            @Override
+            public void stateChanged(ChangeEvent e) {
+
+                SwingUtilities.invokeLater(new Runnable(){
+
+                    @Override
+                    public void run() {
+
+                         if(txtNombre.getCaretPosition() != 0 && nomVacio == true){
+                   txtNombre.setCaretPosition(0);
                }
 
                     }
@@ -253,9 +343,75 @@ public class Ventana{
             }
         });
         
+        
        
 
     }
+
+    
+    //METODOS DEL WT:
+            private boolean WTconFocus(FocusEvent e, JTextField t, String wm, boolean a) { //Si hay focus y sale "Ingrese Apellido", dejamos el curso en 0.
+                if(t.getText().equals(wm)){
+                t.setText(wm);
+                t.setCaretPosition(0); //CARET ES LA POSICIÓN DEL CURSO DEL TEXTO
+                //passVacio = true; //BOOLEANO PARA CORROBORAR SI esta vacío
+                t.setFont(new Font("verdana", Font.ITALIC, 12));
+                t.setForeground(Color.GRAY);
+                return true;
+                }
+                else{
+                    return a;
+                }
+        
+            }
+            
+            
+            
+            private boolean WTsinFocus(FocusEvent e, JTextField t, String wm, boolean a){
+                if(t.getText().equals("")){    
+                    t.setText(wm);
+                    t.setFont(new Font("verdana", Font.ITALIC, 12));
+                    // passVacio = true;  
+                    t.setForeground(Color.GRAY); 
+                    return true;
+                }
+             else{
+                 return a;
+             }
+            }
+            
+            
+            private boolean WTcorroborar(JTextField t, String wm, boolean a){ //PARA QUE FICNOE BUEN HAY Q HACER EL invokeLater.
+                Runnable doHighlight = new Runnable() {
+                    
+                    @Override
+                    public void run() {
+                        if(!t.getText().equals(wm) && a == true){
+                            String texto = t.getText();
+                            texto = texto.replace(wm, "");
+                            t.setText(texto);
+                            t.setFont(new Font("verdana", Font.PLAIN, 12)); 
+                            t.setForeground(Color.BLACK);
+ 
+                        }
+                    }
+                };
+                
+                
+                 
+                if(!t.getText().equals(wm) && a == true){       
+                    SwingUtilities.invokeLater(doHighlight);
+                    return false;         
+                }
+                  
+                  else{
+                    SwingUtilities.invokeLater(doHighlight);
+                      return a;
+                  }
+                  
+            }
+            
+            
     
     
 }
